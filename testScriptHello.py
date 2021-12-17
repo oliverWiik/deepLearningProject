71e69e9ee865dcb1d03c231def9d6cded6786c01
oliverWiik <54854942+oliverWiik@users.noreply.github.com>
2021-12-17 13:12:59 +0100

    Update testScriptHello.py

--- testScriptHello.py
+++ testScriptHello.py
@@ -5,5 +5,12 @@ Created on Fri Dec 17 12:42:43 2021
 @author: olive
 Test script
 """
-
 print('Hello World')
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
