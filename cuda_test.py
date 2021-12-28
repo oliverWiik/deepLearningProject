c7fa2621d0f4110fa9388f70529186d4dcfc3182
BlibberBlobber <Mortenvorborg@hotmail.com>
2021-12-28 19:43:00 +0100

    .

--- cuda_test.py
+++ cuda_test.py
@@ -7,10 +7,24 @@ print("Running GPU.") if use_cuda else print("No GPU available.")
 with open('results.txt', 'w') as f:
 	f.write("This is a test run from a HPC node..")
 
-import  git 
 
-repo = git.Repo('deepLearningProject')
-repo.git.add('results.txt')
-repo.git.commit('-m', 'commit message from python script', author='mortenvorborg@hotmail.com')
-origin = repo.remote(name='origin')
-origin.push()
+import git, os
+repo = git.Repo.clone_from("https://github.com/oliverWiik/deepLearningProject.git", os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'repo'), branch='master')
+#repo.index.add(["results.txt"])
+#repo.index.commit("This is a test commit message form HPC")
+master = repo.heads.master
+master.add(["results.txt"])
+master.commit("Tester test")
+master.push()
+
+#from git import Repo
+#Repo.clone_from("https://github.com/oliverWiik/deepLearningProject.git", "Desktop")
+
+#import git 
+
+#repo = git.Repo('deepLearningProject')
+#git.add('results.txt')
+#git.commit('-m', 'commit message from python script', author='mortenvorborg@hotmail.com')
+
+#origin = remote(name='origin')
+#origin.push()
