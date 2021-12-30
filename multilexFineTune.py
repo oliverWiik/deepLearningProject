6638e44432aca1664b9f2d78b1b2278966397892
oliverWiik <54854942+oliverWiik@users.noreply.github.com>
2021-12-30 14:42:24 +0100

    Update multilexFineTune.py

--- multilexFineTune.py
+++ multilexFineTune.py
@@ -22,7 +22,7 @@ print("Running GPU.") if use_cuda else print("No GPU available.")
 ##### Setup #####
 
 batch_size = 10
-num_epoch = 1
+num_epoch = 3
 
 model = T5ForConditionalGeneration.from_pretrained("ufal/byt5-small-multilexnorm2021-da")
 optimizer = AdamW(model.parameters(), lr=5e-5)
